d6d917e5d640c6b0e9b1c9442e10a36d088f2bc4
schweiz1snow <kazuyukiniwayama@gmail.com>
2021-12-19 17:18:04 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items, dependent: :destroy
   has_many :buyers, dependent: :destroy
+
+  validates :nickname, presence: true
+  validates :email, presence: true, format: { with: ^(?=.*@).*$}
+  validates :encrypted_password, presence: true, format: { with: /^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}$/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
+  validates :birth_date, presence: true
   
 end

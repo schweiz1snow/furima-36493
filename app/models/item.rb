a5a8d0a0c2636b7c34df689edff1fbc9e30808dc
schweiz1snow <kazuyukiniwayama@gmail.com>
2021-12-20 20:29:16 +0900

    修正1-1（テストコード除く）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one :buyer
+  # belongs_to :user
+  # has_one :buyer
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items, dependent: :destroy
-  has_many :buyers, dependent: :destroy
+  # has_many :items
+  # has_many :buyers
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :encrypted_password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "6文字以上半角英数字"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前は全角日本語"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前は全角日本語"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "お名前（カナ）は全角カタカナ"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "お名前（カナ）は全角カタカナ"}
   validates :birth_date, presence: true
   
 end

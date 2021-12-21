08d8c977db6b55ab811fe9c96c8f2cf5e46601bb
schweiz1snow <kazuyukiniwayama@gmail.com>
2021-12-21 21:16:45 +0900

    修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字混合"}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: "お名前は全角日本語"}
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: "お名前は全角日本語"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]/, message: "お名前は全角日本語"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]/, message: "お名前は全角日本語"}
   validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "お名前（カナ）は全角カタカナ"}
   validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "お名前（カナ）は全角カタカナ"}
   validates :birth_date, presence: true

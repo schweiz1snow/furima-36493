0f915e9c19412abf90f285779c41a54437e7de0b
schweiz1snow <kazuyukiniwayama@gmail.com>
2021-12-21 13:52:33 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   # has_many :buyers
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "6文字以上半角英数字"}
+  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "6文字以上半角英数字"}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前は全角日本語"}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前は全角日本語"}
   validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "お名前（カナ）は全角カタカナ"}

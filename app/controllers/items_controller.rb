eee8142150eb36f45f69fa0272c71422f82bd387
schweiz1snow <kazuyukiniwayama@gmail.com>
2021-12-30 16:19:09 +0900

    出品情報保存、エラー表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :explain, :category_id, :condition_id, :payer_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
